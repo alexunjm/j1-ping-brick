af2c787c6174648f0d6290815a3280b2b754393c
alexanderjaramillo <alexunjm@gmail.com>
2020-03-21 02:13:44 -0500

    paleta y movimiento por teclado se agrega paleta se agrega evento para detectar teclado presionado se agrega opción para que se permita la repetición de la tecla presionada se valida que no salga de la pantalla y las acciones de teclado

--- main.py
+++ main.py
@@ -28,14 +28,41 @@ class Bolita(pygame.sprite.Sprite):
         # Mover en base a posiciÃ³n actual y velocidad.
         self.rect.move_ip(self.speed)
 
+class Paleta(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        # Cargar imagen
+        self.image = pygame.image.load('img/paleta.png')
+        # Obtener rectÃ¡ngulo de la imagen
+        self.rect = self.image.get_rect()
+        # PosiciÃ³n inicial centrada en pantalla en X.
+        self.rect.midbottom = (ANCHO / 2, ALTO - 20)
+        # Establecer velocidad inicial.
+        self.speed = [0, 0]
+
+    def update(self, evento):
+        # Buscar si se presionÃ³ flecha izquierda.
+        if evento.key == pygame.K_LEFT and self.rect.left > 0:
+            self.speed = [-5, 0]
+        # Si se presionÃ³ flecha derecha.
+        elif evento.key == pygame.K_RIGHT and self.rect.right < ANCHO:
+            self.speed = [5, 0]
+        else:
+            self.speed = [0, 0]
+        # Mover en base a posiciÃ³n actual y velocidad.
+        self.rect.move_ip(self.speed)
+
 # Inicializando pantalla.
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 # Configurar tÃ­tulo de pantalla.
 pygame.display.set_caption('Juego de ladrillos')
 # Crear el reloj.
 reloj = pygame.time.Clock()
+# Ajustar repeticiÃ³n de evento de tecla presionada.
+pygame.key.set_repeat(30)
 
 bolita = Bolita()
+jugador = Paleta()
 
 while True:
     # Establecer FPS.
@@ -47,14 +74,18 @@ while True:
         if evento.type == pygame.QUIT:
             # cerrar el videojuego.
             sys.exit()
-
-    # Rellenar la pantalla.
-    pantalla.fill(color_azul)
+        # Buscar eventos del teclado,
+        elif evento.type == pygame.KEYDOWN:
+            jugador.update(evento)
 
     # Actualizar posiciÃ³n de la bolita.
     bolita.update()
-    
+
+    # Rellenar la pantalla.
+    pantalla.fill(color_azul)
     # Dibujar bolita en pantalla.
     pantalla.blit(bolita.image, bolita.rect)
+    # Dibujar jugador en pantalla.
+    pantalla.blit(jugador.image, jugador.rect)
     # Actualizar los elementos en pantalla.
     pygame.display.flip()
